b6bdac223aa81d08f75aae2e86d5498d1f0aa8ba
DezkaReid <elmaildeldezkareid@gmail.com>
2017-01-30 01:40:52 -0600

    Added middleware with schema and endpoint graphql

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import graphqlHttp from 'express-graphql';
+import schemas from './schemas';
 
 const app = express();
 mongoose.Promise = global.Promise;
+
+app.use('/graphql', graphqlHttp({
+	schema : schemas,
+	rootValue : global,
+	graphiql : true,
+}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/pokemons');
 app.listen(7000);
