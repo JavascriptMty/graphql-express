4795b25474700e7f25ffc66232bb04e4e429f1f5
DezkaReid <elmaildeldezkareid@gmail.com>
2017-02-01 01:04:15 -0600

    exported input for category

--- types/index.js
+++ types/index.js
@@ -1,9 +1,11 @@
 import CategoryType from './CategoryType';
+import CategoryInputType from './CategoryInputType';
 import AttackType from './AttackType';
 import PokemonType from './PokemonType';
 
 export {
 	CategoryType,
+	CategoryInputType,
 	AttackType,
 	PokemonType,
 }
